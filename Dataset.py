4f3cad48f208f7ff6c6de8211d3a793abf21f89d
Bhipanshu Dhupar <91373531+Bhipanshu@users.noreply.github.com>
2023-10-02 01:28:43 +0530

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -19,7 +19,7 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
-data_dir = '/home/dgxfac14/Work/plantvillage dataset/color'
+data_dir = 'data_dir'
 
 dataset = ImageFolder(data_dir, transform=transform)
 
